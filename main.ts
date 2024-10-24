be67f9dcac0188125adf0712243999e4c23d3c5e
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2024-10-24 17:38:52 +0200

    Acabat

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    music.setTempo(500)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        music.play(music.stringPlayable("E F G A G F E D ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    music.setTempo(500)
 })
